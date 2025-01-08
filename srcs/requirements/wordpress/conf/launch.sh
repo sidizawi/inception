d4454e50173b0614d2537b0590ee7e90a14849e8
sidizawi <zawisidim@gmail.com>
2025-01-08 15:47:13 +0100

    continue

--- srcs/requirements/wordpress/conf/launch.sh
+++ srcs/requirements/wordpress/conf/launch.sh
@@ -8,12 +8,12 @@ sed -E 's/\{\{\s*([A-Za-z_][A-Za-z0-9_]*)\s*\}\}/\$\1/g' "wp-config.php.tmp" > /
 
 rm wp-config.php.tmp
 
-sed -i '/^listen = /c\listen = 0.0.0.0:9000' /etc/php/7.4/fpm/pool.d/www.conf
-sed -i '/php_admin_value[error_log]/c\php_admin_value[error_log] = \/var\/log\/php7.4-fpm.log' /etc/php/7.4/fpm/pool.d/www.conf
+# sed -i '/^listen = /c\listen = 0.0.0.0:9000' /etc/php/7.4/fpm/pool.d/www.conf
+# sed -i '/php_admin_value[error_log]/c\php_admin_value[error_log] = \/var\/log\/php7.4-fpm.log' /etc/php/7.4/fpm/pool.d/www.conf
 
-echo "error_log = /var/log/php7.4-fpm.log" >> /etc/php/7.4/fpm/php.ini
-echo "chdir = /var/www/html" >> /etc/php/7.4/fpm/pool.d/www.conf
-echo "php_admin_flag[log_errors] = on" >> /etc/php/7.4/fpm/pool.d/www.conf
+# echo "error_log = /var/log/php7.4-fpm.log" >> /etc/php/7.4/fpm/php.ini
+# echo "chdir = /var/www/html" >> /etc/php/7.4/fpm/pool.d/www.conf
+# echo "php_admin_flag[log_errors] = on" >> /etc/php/7.4/fpm/pool.d/www.conf
 
 
 php-fpm7.4 -F
