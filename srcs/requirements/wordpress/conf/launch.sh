c83dc615ffa073395ed844156bea3faf8d9501ad
sidizawi <zawisidim@gmail.com>
2025-01-11 20:13:56 +0100

    continue

--- srcs/requirements/wordpress/conf/launch.sh
+++ srcs/requirements/wordpress/conf/launch.sh
@@ -1,12 +1,12 @@
 # exit on errror
 set -e
 
-echo $(curl -s https://api.wordpress.org/secret-key/1.1/salt/) >> wp-config.php
+# echo $(curl -s https://api.wordpress.org/secret-key/1.1/salt/) >> wp-config.php
 
-envsubst < wp-config.php > wp-config.php.tmp
-sed -E 's/\{\{\s*([A-Za-z_][A-Za-z0-9_]*)\s*\}\}/\$\1/g' "wp-config.php.tmp" > /var/www/html/wp-config.php
+# envsubst < wp-config.php > wp-config.php.tmp
+# sed -E 's/\{\{\s*([A-Za-z_][A-Za-z0-9_]*)\s*\}\}/\$\1/g' "wp-config.php.tmp" > /var/www/html/wp-config.php
 
-rm wp-config.php.tmp
+# rm wp-config.php.tmp
 
 # sed -i '/^listen = /c\listen = 0.0.0.0:9000' /etc/php/7.4/fpm/pool.d/www.conf
 # sed -i '/php_admin_value[error_log]/c\php_admin_value[error_log] = \/var\/log\/php7.4-fpm.log' /etc/php/7.4/fpm/pool.d/www.conf
