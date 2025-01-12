0946401c4de443a44c452981f47b508cf39bf286
sidizawi <zawisidim@gmail.com>
2025-01-12 21:14:28 +0100

    continue

--- srcs/requirements/wordpress/conf/launch.sh
+++ srcs/requirements/wordpress/conf/launch.sh
@@ -8,12 +8,14 @@ set -e
 
 # rm wp-config.php.tmp
 
-# sed -i '/^listen = /c\listen = 0.0.0.0:9000' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i '/^listen = /c\listen = 0.0.0.0:9000' /etc/php/7.4/fpm/pool.d/www.conf
 # sed -i '/php_admin_value[error_log]/c\php_admin_value[error_log] = \/var\/log\/php7.4-fpm.log' /etc/php/7.4/fpm/pool.d/www.conf
 
 # echo "error_log = /var/log/php7.4-fpm.log" >> /etc/php/7.4/fpm/php.ini
 # echo "chdir = /var/www/html" >> /etc/php/7.4/fpm/pool.d/www.conf
 # echo "php_admin_flag[log_errors] = on" >> /etc/php/7.4/fpm/pool.d/www.conf
 
+mkdir -p /var/www/html
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 
 php-fpm7.4 -F
