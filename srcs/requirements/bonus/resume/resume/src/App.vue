1fd60b01eef30a0cf41739a686da3d7159fdb3f8
sidizawi <zawisidim@gmail.com>
2025-02-11 22:53:35 +0100

    starting static site

---
+++ srcs/requirements/bonus/resume/resume/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import Body from '@/components/Body.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+  <Header></Header>
+  <Body></Body>
+  <Footer></Footer>
+</template>
---
+++ srcs/requirements/bonus/resume/resume/src/components/Body.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>body</h1>
+</template>
